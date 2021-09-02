90aac6e9a1309b2d5772e31bd4058093fc7fa7f3
respinoza <respinoza@minsa.gob.ni>
2021-09-02 14:42:32 -0600

    se realiza plantilla principal

---
+++ curso-angular/src/app/shared/pasajero/pasajero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pasajero',
+  templateUrl: './pasajero.component.html',
+  styleUrls: ['./pasajero.component.scss']
+})
+export class PasajeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ curso-angular/src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { MaterialModule } from 'src/modules/material/material.module';
+import { PasajeroComponent } from './pasajero/pasajero.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsuarioComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoPageFoundComponent,
+    PasajeroComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    NoPageFoundComponent
+  ]
+})
+export class SharedModule { }
